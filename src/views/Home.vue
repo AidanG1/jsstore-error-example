d7c5a246c7488bf35ddb9dd2e17929ab987a7bd4
Aidan Gerber <agentaidan@gmail.com>
2021-06-13 21:36:07 -0700

    fixed commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='home'>
+    <img alt='Vue logo' src='../assets/logo.png' />
+    <StudentGrid
+      :students='students'
+      @add-item='addStudent'
+      @remove-item='refreshStudent'
+      @update-item='refreshStudent'
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import StudentGrid from '../components/StudentGrid.vue'
+import { initJsStore } from '../service/idb_service'
+import { StudentService } from '../service/student_service'
+import { Global } from '../global'
 export default {
-  name: 'Home',
+  name: 'app',
   components: {
-    HelloWorld
+    StudentGrid
+  },
+  async beforeCreate () {
+    try {
+      const isDbCreated = await initJsStore()
+      if (isDbCreated) {
+        console.log('db created')
+        // prefill database
+      } else {
+        console.log('db opened')
+      }
+    } catch (ex) {
+      console.error(ex)
+      alert(ex.message)
+      Global.isIndexedDbSupported = false
+    }
+  },
+  mounted () {
+    this.refreshStudent()
+  },
+  data () {
+    return {
+      students: []
+    }
+  },
+  methods: {
+    async refreshStudent () {
+      this.students = await new StudentService().getStudents()
+    },
+    addStudent (student) {
+      this.students.push(student)
+    }
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
